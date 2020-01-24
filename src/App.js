8b2bc340461a3968fdddc53e4dc381f61009a596
cnvs_my <mynguyen@Macintoshs-MacBook-Pro-5.local>
2020-01-24 15:58:16 +0700

    Add data to reactjs

--- src/App.js
+++ src/App.js
@@ -4,17 +4,48 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+      data: []
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    // Read res from service via Socket IO
+    var res = window.q;
+    var resArray = [
+      { gtbl_id: res.gtbl_id },
+      { gtbl_id_enc: res.gtbl_id_enc },
+      { plyr_id_enc: res.plyr_id_enc },
+      { ws_host: res.ws_host },
+      { gtbl_name: res.gtbl_name },
+      { currency: res.currency },
+      { sessionID: res.sessionID },
+      { currency: res.currency },
+      { gtbl_isessionIDd: res.sessionID }
+    ];
+    this.setState({
+      data: [...this.state.data, resArray]
+    });
+  }
 
   render() {
+    const { data } = this.state;
+    console.log(data.gtbl_id);
+    let render;
+    if (data == null) {
+      render = <div>No data</div>;
+    } else {
+      render = (
+        <div>
+          <div>test: {data.gtbl_id}</div>
+          <div></div>
+          <div></div>
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <div>
-          <h2>ReactJS app is generated here</h2>
-        </div>
+        <div>test</div>
       </div>
     );
   }
