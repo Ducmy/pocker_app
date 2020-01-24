ccf903d75f63c08f65a94f715595b0b3ecaf1402
cnvs_my <mynguyen@Macintoshs-MacBook-Pro-5.local>
2020-01-24 22:15:06 +0700

    Add socker:res message to react js

--- src/App.js
+++ src/App.js
@@ -5,47 +5,70 @@ class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      data: []
+      gtbl_id: "",
+      gtbl_id_enc: "",
+      plyr_id_enc: "",
+      ws_host: "",
+      ws_port: "",
+      gtbl_name: "",
+      currency: "",
+      sessionID: ""
     };
   }
 
   componentDidMount() {
     // Read res from service via Socket IO
     var res = window.q;
-    var resArray = [
-      { gtbl_id: res.gtbl_id },
-      { gtbl_id_enc: res.gtbl_id_enc },
-      { plyr_id_enc: res.plyr_id_enc },
-      { ws_host: res.ws_host },
-      { gtbl_name: res.gtbl_name },
-      { currency: res.currency },
-      { sessionID: res.sessionID },
-      { currency: res.currency },
-      { gtbl_isessionIDd: res.sessionID }
-    ];
     this.setState({
-      data: [...this.state.data, resArray]
+      gtbl_id: res.gtbl_id,
+      gtbl_id_enc: res.gtbl_id_enc,
+      plyr_id_enc: res.plyr_id_enc,
+      ws_host: res.ws_host,
+      ws_port: res.ws_port,
+      gtbl_name: res.gtbl_name,
+      currency: res.currency,
+      sessionID: res.sessionID
+    });
+    console.log(res.sessionID);
+    // window.sendMsg("setSession", [res.gtbl_id]);
+    window.socket.on("connect", function(msg) {
+      //TODO take care not to create duplicate listeners - use socket.hasListeners to check
+      window.socket.on("message", window.receiveMsg);
+      //	socket.on('disconnect', ...
+      //	socket.on('connect_failed', ...
+      //	socket.on('error', ...
+
+      // set up the session - this will trigger the server to send all the table details:
+      window.sendMsg("setSession", [window.q.gtbl_id_enc]);
     });
   }
 
   render() {
-    const { data } = this.state;
-    console.log(data.gtbl_id);
-    let render;
-    if (data == null) {
-      render = <div>No data</div>;
-    } else {
-      render = (
-        <div>
-          <div>test: {data.gtbl_id}</div>
-          <div></div>
-          <div></div>
-        </div>
-      );
-    }
+    const {
+      gtbl_id,
+      gtbl_id_enc,
+      plyr_id_enc,
+      ws_host,
+      ws_port,
+      gtbl_name,
+      currency,
+      sessionID
+    } = this.state;
     return (
       <div className="App">
-        <div>test</div>
+        <div>
+          <h1>Read socket IO react js app</h1>
+        </div>
+        <div>gtbl_id:{gtbl_id}</div>
+        <div>gtbl_id_enc:{gtbl_id_enc}</div>
+        <div>plyr_id_enc:{plyr_id_enc}</div>
+        <div>ws_host:{ws_host}</div>
+        <div>ws_port:{ws_port}</div>
+        <div>gtbl_name:{gtbl_name}</div>
+        <div>currency:{currency}</div>
+        <div>
+          sessionID:<span className="special">{sessionID}</span>
+        </div>
       </div>
     );
   }
