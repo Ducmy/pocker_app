2b0ea48dec2d66e6e63d512c1e6d9d756d598cc7
cnvs_my <mynguyen@Macintoshs-MacBook-Pro-5.local>
2020-01-25 21:45:32 +0700

    Add message responsive

--- src/App.js
+++ src/App.js
@@ -1,74 +1,65 @@
 import React, { Component } from "react";
+import io from "socket.io-client";
 import "./App.css";
 import { connect } from "react-redux";
+
+const receiveMsg = text => {
+  var params = text.split("|"); //.map(p => Base64.Decode(p)); // we are not using b64 now
+  var message = params.shift(); // message, eg. playerSitOut, clearTable
+  console.log("Recevied " + message + ", with params " + params.join("; "));
+};
+
+const sendMsg = (action, params) => {
+  // params is an array
+  params.unshift(action);
+  var text = params.map(p => p.toString()).join("|"); // we are not using base64 now, just convert them to string
+  console.log("Sending " + params.join(", ") + " encoded as " + text);
+  socket.send(text);
+};
+
+const socket = io.connect(
+  "https://" + window.q.ws_host + ":" + window.q.ws_port
+);
 
 class App extends Component {
   constructor(props) {
     super();
-    this.state = {
-      gtbl_id: "",
-      gtbl_id_enc: "",
-      plyr_id_enc: "",
-      ws_host: "",
-      ws_port: "",
-      gtbl_name: "",
-      currency: "",
-      sessionID: "",
-      params: [],
-      messages: []
-    };
+    this.state = { msg: "", chat: [] };
   }
 
   componentDidMount() {
-    var varOnReact = "Value On ReactJS";
-    var receiveMsg = text => {
+    // Read res from service via Socket IO
+    // socket.on("message", receiveMsg);
+    socket.on("message", text => {
       var params = text.split("|"); //.map(p => Base64.Decode(p)); // we are not using b64 now
       var message = params.shift(); // message, eg. playerSitOut, clearTable
-      // console.log("Recevied " + message + ", with params " + params.join("; "));
-      varOnReact = message;
-      //window.store.dispatch({ type: "UPDATE_USER", payload: "mynguyen" });
-    };
-    console.log(receiveMsg);
-    window.socket.on("connect", function(msg) {
-      //TODO take care not to create duplicate listeners - use socket.hasListeners to check
-      window.socket.on("message", receiveMsg);
-      console.log(receiveMsg);
-      //	socket.on('disconnect', ...
-      //	socket.on('connect_failed', ...
-      //	socket.on('error', ...
-
-      // set up the session - this will trigger the server to send all the table details:
-      window.sendMsg("setSession", [window.q.gtbl_id_enc]);
+      console.log("Recevied " + message + ", with params " + params.join("; "));
+      this.setState({
+        chat: [...this.state.chat, params]
+      });
     });
-
-    // Read res from service via Socket IO
-    var res = window.q;
-    this.setState({
-      gtbl_id: res.gtbl_id,
-      gtbl_id_enc: res.gtbl_id_enc
-    });
-    console.log(varOnReact);
   }
 
-  handleData = () => {
-    this.props.dispatch({ type: "UPDATE_USER", payload: "mynguyen" }); // Update user state on store
+  onMessageSubmit = () => {
+    // set up the session - this will trigger the server to send all the table details:
+    sendMsg("setSession", [window.q.gtbl_id_enc]);
   };
 
-  render() {
-    const { gtbl_id, sessionID } = this.state;
-    const { user } = this.props;
+  // handleData = () => {
+  //   this.props.dispatch({ type: "UPDATE_USER", payload: "mynguyen" }); // Update user state on store
+  // };
 
+  render() {
+    // const { user } = this.props;
+    console.log(this.state.chat);
     return (
       <div className="App">
         <div>
-          <h1>Read socket IO react js app</h1>
+          <h1>Reasocke tessreact js app</h1>
         </div>
-        <div>Data from Redux store {user}</div>
-        <div>gtbl_id:{gtbl_id}</div>
-        <div>
-          sessionID:<span className="special">{sessionID}</span>
-        </div>
-        <button onClick={this.handleData.bind(this)}>Click to change</button>
+        <button onClick={this.onMessageSubmit}>Send Message</button>
+        {/* <div>Data from Redux store {user}</div>
+        <button onClick={this.handleData.bind(this)}>Click to change</button> */}
       </div>
     );
   }
